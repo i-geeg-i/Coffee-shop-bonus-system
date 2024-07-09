3994f4a7ceca2555470fa40787309443f5305a0f
iJeannne <166218229+iJeannne@users.noreply.github.com>
2024-07-09 15:09:49 +0300

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Auth from "../api/auth/login";
 
 export default function Profile() {
     return( <div>
-        <h1 className="main-title">Profile</h1>
+        <h1 className="main-title">Profile</h1>       
+        <Auth />
         <Link href="/">Go back</Link>
         </div>
     )
